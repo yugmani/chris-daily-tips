fc911af649b57e494b43a6dd618ac2b2508ddfd9
yugmani <yugmani@yahoo.com>
2021-05-09 00:16:48 -0700

    finalized

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 let greeting = document.querySelector('.greeting');
-greeting.style.display = none;
 
-function displayGreeting() {
+function displayCode() {
   let code = `
   <div class="greeting">
     <p>Hello World!</p>
   </div>
   `;
-
-  // greeting.style.display = invisible;
+  //   greeting.style.display = "none";
+  let codeDiv = document.querySelector('.code');
+  codeDiv.textContent = code;
 }
 
-displayGreeting();
+displayCode();
+
+function innerFunction() {
+  let html = `<h3>innerHTML</h3><div class="code">`;
+  html += `document.querySelector('.greeting').innerHTML</div>`;
+  html += `<h4>Output</h4>`;
+
+  html += greeting.innerHTML;
+  document.getElementById('inner').innerHTML = html;
+}
+
+innerFunction();
+
+function outerFunction() {
+  let htm = `<h3>outerHTML</h3><div class="code">`;
+  htm += `document.querySelector('.greeting').outerHTML</div>`;
+  htm += `<h4>Output</h4>`;
+
+  let html = greeting.outerHTML;
+
+  document.querySelector('.outer-code').innerHTML = htm;
+  document.querySelector('.outer-html').textContent = html;
+}
+
+outerFunction();
