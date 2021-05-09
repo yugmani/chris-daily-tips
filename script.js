afb229aacbe7be6b4ae8072de90fc976d17ec13a
yugmani <yugmani@yahoo.com>
2021-05-08 22:59:24 -0700

    modifying display

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let greeting = document.querySelector('.greeting');
+greeting.style.display = none;
 
+function displayGreeting() {
+  let code = `
+  <div class="greeting">
+    <p>Hello World!</p>
+  </div>
+  `;
+
+  // greeting.style.display = invisible;
+}
+
+displayGreeting();
